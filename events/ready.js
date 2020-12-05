ce9776b42a02df3736a3730a475f46c4bceb9444
Steven Rafferty <stevenrafferty2@protonmail.com>
2020-12-05 21:12:10 +0000

    Remove unnecessary variable

--- events/ready.js
+++ events/ready.js
@@ -3,14 +3,13 @@ module.exports = async (client) => {
     const guildIds = client.guilds.cache.map(({ id }) => id);
     try {
         // Get channelIds
-        const channelIdsPromise = await guildIds.reduce(async (acc, guildId) => {
+        const channelIds = await guildIds.reduce(async (acc, guildId) => {
             const guild = await client.guilds.fetch(guildId);
             const guildChannels = guild.channels.cache.filter(({ type }) => type === 'text');
             const guildChannelIds = guildChannels.map(({ id }) => id);
             acc.push(...guildChannelIds);
             return acc;
         }, []);
-        const channelIds = await channelIdsPromise;
         await channelIds.forEach(async channelId => {
             const channel = await client.channels.cache.get(channelId);
             await channel.messages.fetch();
